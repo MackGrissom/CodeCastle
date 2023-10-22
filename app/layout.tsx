643b3fd150129144846c484c6f83d034f59313ea
Mack Grissom <mcintyregrissom@gmail.com>
2023-10-23 05:02:24 +0700

    changed color theme, added assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,26 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
-export const metadata = {
-  title: "Next.js 13 with Clerk",
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "CodeCastle",
+  description:
+    "Welcome to Code Castle, your ultimate destination for all things developer-related. Explore the latest developer news, tackle LeetCode-style challenges, engage in StackOverflow forums, and connect with like-minded enthusiasts. Join us today and reign over the kingdom of code!",
+  icons: "",
 };
 
 export default function RootLayout({
@@ -10,9 +29,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+    appearance={{
+      elements: {
+        formButtonPrimary:'primary-gradient',
+        footerActionLink:'primary-text-gradient hover:text-primary-500'
+      }
+    }}>
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
